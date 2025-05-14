a37f789f7157945b889826e44b0fcd17faf64d57
Soumyajit <4soumya19@gmail.com>
2025-05-14 17:57:28 +0530

    error free

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -2,8 +2,8 @@ import styles from "./HeroStyles.module.css";
 import heroImg from "../../assets/Design uten navn.png";
 import sun from "../../assets/sun.svg";
 import moon from "../../assets/moon.svg";
-import twitterLight from "../../assets/twitter-light.svg";
-import twitterDark from "../../assets/twitter-dark.svg";
+// import twitterLight from "../../assets/twitter-light.svg";
+// import twitterDark from "../../assets/twitter-dark.svg";
 import githubLight from "../../assets/github-light.svg";
 import githubDark from "../../assets/github-dark.svg";
 import linkedinLight from "../../assets/linkedin-light.svg";
@@ -11,13 +11,13 @@ import linkedinDark from "../../assets/linkedin-dark.svg";
 import { useTheme } from "../../common/ThemeContext";
 
 // import SoumyaResume from "../../assets/SoumyajitFrontEndDeveloperFeb2.pdf";
-import SoumyaFrontResume from "../../assets/Soumyajit_FrontEnd_Mar.pdf";
+// import SoumyaFrontResume from "../../assets/Soumyajit_FrontEnd_Mar.pdf";
 import SoumyaResumeT from "../../assets/Soumyajit_resume_t1.pdf";
 const Hero = () => {
   const { theme, toggleTheme } = useTheme();
 
   const themeIcon = theme === "light" ? sun : moon;
-  const twitterIcon = theme === "light" ? twitterLight : twitterDark;
+  // const twitterIcon = theme === "light" ? twitterLight : twitterDark;
   const githubIcon = theme === "light" ? githubLight : githubDark;
   const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
   return (
